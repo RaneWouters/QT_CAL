ae792fe4e2d9e3714f82443b7e0958f7c3e8316f
RaneWouters <1540656951@qq.com>
2021-05-19 21:45:09 +0800

    demo

--- CAL_zh_CN.ts
+++ CAL_zh_CN.ts
@@ -4,208 +4,208 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.ui" line="14"/>
+        <location filename="mainwindow.ui" line="20"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="54"/>
+        <location filename="mainwindow.ui" line="1037"/>
         <source>2nd</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="70"/>
+        <location filename="mainwindow.ui" line="1067"/>
         <source>π</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="86"/>
+        <location filename="mainwindow.ui" line="1097"/>
         <source>sin</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="102"/>
+        <location filename="mainwindow.ui" line="1127"/>
         <source>cos</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="118"/>
+        <location filename="mainwindow.ui" line="1157"/>
         <source>tan</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="138"/>
+        <location filename="mainwindow.ui" line="86"/>
         <source>x^2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="154"/>
+        <location filename="mainwindow.ui" line="116"/>
         <source>1/x</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="170"/>
+        <location filename="mainwindow.ui" line="146"/>
         <source>ln</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="186"/>
+        <location filename="mainwindow.ui" line="176"/>
         <source>exp</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="202"/>
+        <location filename="mainwindow.ui" line="206"/>
         <source>mod</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="222"/>
+        <location filename="mainwindow.ui" line="240"/>
         <source>2√x</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="238"/>
+        <location filename="mainwindow.ui" line="270"/>
         <source>(</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="254"/>
+        <location filename="mainwindow.ui" line="301"/>
         <source>)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="270"/>
+        <location filename="mainwindow.ui" line="338"/>
         <source>&lt;--</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="286"/>
+        <location filename="mainwindow.ui" line="383"/>
         <source>/</source>
         <oldsource>%</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="306"/>
+        <location filename="mainwindow.ui" line="417"/>
         <source>x^y</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="322"/>
+        <location filename="mainwindow.ui" line="446"/>
         <source>7</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="338"/>
+        <location filename="mainwindow.ui" line="475"/>
         <source>8</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="354"/>
+        <location filename="mainwindow.ui" line="504"/>
         <source>9</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="370"/>
+        <location filename="mainwindow.ui" line="534"/>
         <source>*</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="390"/>
+        <location filename="mainwindow.ui" line="568"/>
         <source>10^x</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="406"/>
+        <location filename="mainwindow.ui" line="597"/>
         <source>4</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="422"/>
+        <location filename="mainwindow.ui" line="626"/>
         <source>5</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="438"/>
+        <location filename="mainwindow.ui" line="655"/>
         <source>6</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="454"/>
+        <location filename="mainwindow.ui" line="685"/>
         <source>-</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="474"/>
+        <location filename="mainwindow.ui" line="719"/>
         <source>|x|</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="490"/>
+        <location filename="mainwindow.ui" line="748"/>
         <source>1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="506"/>
+        <location filename="mainwindow.ui" line="793"/>
         <source>2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="522"/>
+        <location filename="mainwindow.ui" line="822"/>
         <source>3</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="538"/>
+        <location filename="mainwindow.ui" line="852"/>
         <source>+</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="558"/>
+        <location filename="mainwindow.ui" line="886"/>
         <source>n!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="574"/>
+        <location filename="mainwindow.ui" line="915"/>
         <source>+/-</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="590"/>
+        <location filename="mainwindow.ui" line="944"/>
         <source>0</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="606"/>
+        <location filename="mainwindow.ui" line="973"/>
         <source>.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="622"/>
+        <location filename="mainwindow.ui" line="1003"/>
         <source>=</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="658"/>
+        <location filename="mainwindow.ui" line="1183"/>
         <source>MC</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="674"/>
+        <location filename="mainwindow.ui" line="1205"/>
         <source>MR</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="690"/>
+        <location filename="mainwindow.ui" line="1227"/>
         <source>M+</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="706"/>
+        <location filename="mainwindow.ui" line="1249"/>
         <source>M-</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="722"/>
+        <location filename="mainwindow.ui" line="1271"/>
         <source>MS</source>
         <translation type="unfinished"></translation>
     </message>
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -74,7 +74,7 @@ void MainWindow::on_Square_clicked()
     UserText += "Q";
     UserShowText += "^2";
     Count.push_back(2);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_Sqrt_clicked()
@@ -82,7 +82,7 @@ void MainWindow::on_Sqrt_clicked()
     UserText += "q";
     UserShowText += "sqrt";
     Count.push_back(4);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_Pow_clicked()
@@ -90,7 +90,7 @@ void MainWindow::on_Pow_clicked()
     UserText += "^";
     UserShowText += "^";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_TenPow_clicked()
@@ -98,7 +98,7 @@ void MainWindow::on_TenPow_clicked()
     UserText += "p";
     UserShowText += "10^";
     Count.push_back(3);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_Abs_clicked()
@@ -106,7 +106,7 @@ void MainWindow::on_Abs_clicked()
     UserText += "|";
     UserShowText += "abs";
     Count.push_back(3);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_Fractorial_clicked()
@@ -114,7 +114,7 @@ void MainWindow::on_Fractorial_clicked()
     UserText += "!";
     UserShowText += "!";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_Pi_clicked()
@@ -122,7 +122,7 @@ void MainWindow::on_Pi_clicked()
     UserText += "3.141592653";
     UserShowText += "pi";
     Count.push_back(2);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_reciprocal_clicked()
@@ -130,7 +130,7 @@ void MainWindow::on_reciprocal_clicked()
     UserText += "r";
     UserShowText += "1/";
     Count.push_back(2);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_leftparenthsis_clicked()
@@ -138,7 +138,7 @@ void MainWindow::on_leftparenthsis_clicked()
     UserText += "(";
     UserShowText += "(";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_rightparenthsis_clicked()
@@ -146,7 +146,7 @@ void MainWindow::on_rightparenthsis_clicked()
     UserText += ")";
     UserShowText += ")";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_Ln_clicked()
@@ -154,7 +154,7 @@ void MainWindow::on_Ln_clicked()
     UserText += "l";
     UserShowText += "ln";
     Count.push_back(2);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_Exp_clicked()
@@ -162,7 +162,7 @@ void MainWindow::on_Exp_clicked()
     UserText += "e";
     UserShowText += "exp";
     Count.push_back(3);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_Mod_clicked()
@@ -170,7 +170,7 @@ void MainWindow::on_Mod_clicked()
     UserText += "%";
     UserShowText += "%";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_Divide_clicked()
@@ -178,7 +178,7 @@ void MainWindow::on_Divide_clicked()
     UserText += "/";
     UserShowText += "/";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_Times_clicked()
@@ -186,7 +186,7 @@ void MainWindow::on_Times_clicked()
     UserText += "*";
     UserShowText += "*";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_Minus_clicked()
@@ -194,7 +194,7 @@ void MainWindow::on_Minus_clicked()
     UserText += "-";
     UserShowText += "-";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_Plus_clicked()
@@ -202,7 +202,7 @@ void MainWindow::on_Plus_clicked()
     UserText += "+";
     UserShowText += "+";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_Sin_clicked()
@@ -211,12 +211,12 @@ void MainWindow::on_Sin_clicked()
         UserText += "s";
         UserShowText += "sin";
         Count.push_back(3);
-        ui->textBrowser->setText(QString::fromStdString(UserShowText));
+        ui->lineEdit->setText(QString::fromStdString(UserShowText));
     } else if (ui->Sin->text() == "sinh") {
         UserText += "S";
         UserShowText += "sinh";
         Count.push_back(4);
-        ui->textBrowser->setText(QString::fromStdString(UserShowText));
+        ui->lineEdit->setText(QString::fromStdString(UserShowText));
     }
 }
 
@@ -226,12 +226,12 @@ void MainWindow::on_Cos_clicked()
         UserText += "c";
         UserShowText += "cos";
         Count.push_back(3);
-        ui->textBrowser->setText(QString::fromStdString(UserShowText));
+        ui->lineEdit->setText(QString::fromStdString(UserShowText));
     } else if (ui->Cos->text() == "cosh") {
         UserText += "C";
         UserShowText += "cosh";
         Count.push_back(4);
-        ui->textBrowser->setText(QString::fromStdString(UserShowText));
+        ui->lineEdit->setText(QString::fromStdString(UserShowText));
     }
 }
 
@@ -241,12 +241,12 @@ void MainWindow::on_Tan_clicked()
         UserText += "t";
         UserShowText += "tan";
         Count.push_back(3);
-        ui->textBrowser->setText(QString::fromStdString(UserShowText));
+        ui->lineEdit->setText(QString::fromStdString(UserShowText));
     } else if (ui->Tan->text() == "tanh") {
         UserText += "T";
         UserShowText += "tanh";
         Count.push_back(4);
-        ui->textBrowser->setText(QString::fromStdString(UserShowText));
+        ui->lineEdit->setText(QString::fromStdString(UserShowText));
     }
 }
 
@@ -255,7 +255,7 @@ void MainWindow::on_negate_clicked()
     UserText += "n";
     UserShowText += "+/-";
     Count.push_back(3);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_equal_clicked()
@@ -269,26 +269,32 @@ void MainWindow::on_equal_clicked()
             throw 1;
         In.Init(UserText);
         //    for(auto q = In.GetVec().begin(); q != In.GetVec().end(); ++q) {
-        //        ui->textBrowser->append(QString::fromStdString(*q));
+        //        ui->lineEdit->append(QString::fromStdString(*q));
         //    }
-        //    ui->textBrowser->append(" ");
+        //    ui->lineEdit->append(" ");
 
         queue<string> target = RPInit(In.GetVec());
         //    while (!target.empty()) {
-        //        ui->textBrowser->append(QString::fromStdString(target.front()));
+        //        ui->lineEdit->append(QString::fromStdString(target.front()));
         //        target.pop();
         //    }
-        //    ui->textBrowser->append(" ");
+        //    ui->lineEdit->append(" ");
 
         double value = RPCalculate(target);
 
         string t = to_string(value);
-        ui->textBrowser->setText(QString::fromStdString(t));
+        ui->lineEdit->setText(QString::fromStdString(t));
+
         UserText.clear();
         UserShowText.clear();
+        Count.clear();
         In.GetVec().clear();
+
+        UserShowText += t;
+        UserText += t;
+        Count.push_back(t.length());
     } catch (...) {
-        ui->textBrowser->setText("ERROR!");
+        ui->lineEdit->setText("ERROR!");
         UserText.clear();
         UserShowText.clear();
         In.GetVec().clear();
@@ -300,7 +306,7 @@ void MainWindow::on_zero_clicked()
     UserText += "0";
     UserShowText += "0";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_one_clicked()
@@ -308,7 +314,7 @@ void MainWindow::on_one_clicked()
     UserText += "1";
     UserShowText += "1";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_two_clicked()
@@ -316,7 +322,7 @@ void MainWindow::on_two_clicked()
     UserText += "2";
     UserShowText += "2";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_three_clicked()
@@ -324,7 +330,7 @@ void MainWindow::on_three_clicked()
     UserText += "3";
     UserShowText += "3";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_four_clicked()
@@ -332,7 +338,7 @@ void MainWindow::on_four_clicked()
     UserText += "4";
     UserShowText += "4";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_five_clicked()
@@ -340,7 +346,7 @@ void MainWindow::on_five_clicked()
     UserText += "5";
     UserShowText += "5";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_six_clicked()
@@ -348,7 +354,7 @@ void MainWindow::on_six_clicked()
     UserText += "6";
     UserShowText += "6";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_seven_clicked()
@@ -356,7 +362,7 @@ void MainWindow::on_seven_clicked()
     UserText += "7";
     UserShowText += "7";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_eight_clicked()
@@ -364,7 +370,7 @@ void MainWindow::on_eight_clicked()
     UserText += "8";
     UserShowText += "8";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_nine_clicked()
@@ -372,7 +378,7 @@ void MainWindow::on_nine_clicked()
     UserText += "9";
     UserShowText += "9";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_point_clicked()
@@ -380,51 +386,73 @@ void MainWindow::on_point_clicked()
     UserText += ".";
     UserShowText += ".";
     Count.push_back(1);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_clean_clicked()
 {
-    for (int q = 0; q < *(Count.end()-1); ++q){
-        if(!UserShowText.empty()){
-            UserShowText.pop_back();
+    if(!Count.empty()){
+        if(IsNum(*(UserText.end()-1))) {
+            for (int q = 0; q < *(Count.end()-1); ++q){
+                if(!UserShowText.empty() && !UserText.empty()){
+                    UserShowText.pop_back();
+                    UserText.pop_back();
+                }
+            }
+        }else {
+            for (int q = 0; q < *(Count.end()-1); ++q){
+                if(!UserShowText.empty() && !UserText.empty()){
+                    UserShowText.pop_back();
+                }
+                UserText.pop_back();
+            }
         }
+        Count.pop_back();
     }
-    UserText.pop_back();
-    Count.pop_back();
-
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_MS_clicked()
 {
-    In.Init(UserText);
-    if (IsNum(*((In.GetVec().end()-1)->end()-1))) {
-        MRegister.MS();
-    } else {
-        ui->textBrowser->setText("ERROR!");
+    if(!UserText.empty()){
+        In.Init(UserText);
+        if (!In.GetVec().empty()){
+            if (IsNum(*((In.GetVec().end()-1)->end()-1))) {
+                MRegister.MS();
+            } else {
+                ui->lineEdit->setText("ERROR!");
+            }
+        }
     }
     In.GetVec().clear();
 }
 
 void MainWindow::on_Mminus_clicked()
 {
-    In.Init(UserText);
-    if (IsNum(*((In.GetVec().end()-1)->end()-1))) {
-        MRegister.MMinus();
-    } else {
-        ui->textBrowser->setText("ERROR!");
+    if(!UserText.empty()){
+        In.Init(UserText);
+        if (!In.GetVec().empty()){
+            if (IsNum(*((In.GetVec().end()-1)->end()-1))) {
+                MRegister.MMinus();
+            } else {
+                ui->lineEdit->setText("ERROR!");
+            }
+        }
     }
     In.GetVec().clear();
 }
 
 void MainWindow::on_Mplus_clicked()
 {
-    In.Init(UserText);
-    if (IsNum(*((In.GetVec().end()-1)->end()-1))) {
-        MRegister.MPlus();
-    } else {
-        ui->textBrowser->setText("ERROR!");
+    if(!UserText.empty()){
+        In.Init(UserText);
+        if (!In.GetVec().empty()){
+            if (IsNum(*((In.GetVec().end()-1)->end()-1))) {
+                MRegister.MPlus();
+            } else {
+                ui->lineEdit->setText("ERROR!");
+            }
+        }
     }
     In.GetVec().clear();
 }
@@ -444,11 +472,11 @@ void MainWindow::on_MR_clicked()
 
     UserText += to_string(value);
     UserShowText += to_string(value);
-    ui->textBrowser->setText(QString::fromStdString(UserShowText));
+    ui->lineEdit->setText(QString::fromStdString(UserShowText));
 }
 
 void MainWindow::on_MC_clicked()
 {
     MRegister.MC();
-    Count.pop_back();
+    //Count.pop_back();
 }
